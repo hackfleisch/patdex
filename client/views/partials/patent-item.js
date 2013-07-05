ad884e815159302525732ce4f8880b0e1d59be77
brentriddell <brentriddell@gmail.com>
2013-07-05 14:51:50 +0200

    adds gizmo iconfont and trash and PDF icons to patents

--- client/views/partials/patent-item.js
+++ client/views/partials/patent-item.js
@@ -6,11 +6,28 @@ Template.patentItem.rendered = function(e) {
 
 Template.patentItem.events({
 
-
+  'click #view-PDF, tap #view-PDF' : function(e) {
+    if(ignoreClick(e)) return;
+    alert("PDF!");
+  }
 
 });
 
 Template.patentItem.helpers({
 
+	pdfLink : function(e) {
+		return this.PDF;
+	},
+
+	searchResults: function(e) {
+		return Session.get('resultStatus');
+	},
+
+  userLoggedIn : function(e) {
+    if(Session.get('requestedUser') === Session.get('currentUser')) {
+      return true;
+    }
+    return false;
+  }
 
 });
